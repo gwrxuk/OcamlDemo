c6404ead8d9227ec7a1ab12a7b0783f4ca9930a7
gwrxuk <gwrx2005@gmail.com>
2024-04-21 20:38:22 +0800

    Update main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,31 @@
+open Angstrom
+
+let parens p = char '(' *> p <* char ')'
+let add = char '+' *> return (+)
+let sub = char '-' *> return (-)
+let mul = char '*' *> return ( * )
+let div = char '/' *> return (/)
+let integer =
+  take_while1 (function '0' .. '9' -> true | _ -> false) >>| int_of_string
+
+let chainl1 e op =
+  let rec go acc =
+    (lift2 (fun f x -> f acc x) op e >>= go) <|> return acc in
+  e >>= fun init -> go init
+
+let expr : int t =
+  fix (fun expr ->
+    let factor = parens expr <|> integer in
+    let term   = chainl1 factor (mul <|> div) in
+    chainl1 term (add <|> sub))
+
+let eval (str:string) : int =
+  match parse_string ~consume:All expr str with
+  | Ok v      -> v
+  | Error msg -> failwith msg
+
+let () = print_int(eval ("1+1"))
+
 let () =
   Dream.run
   @@ Dream.logger
@@ -11,4 +39,11 @@ let () =
       (fun request ->
         Dream.html (Dream.param request "word"));
 
+    Dream.get "/number"
+      (fun _ ->
+        Dream.html (string_of_int (eval ("1+1"))));
+
   ]
+
+
+  
